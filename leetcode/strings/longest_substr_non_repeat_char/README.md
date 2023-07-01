16776758a41bd72febe413d9dfa62cc96a934ce3
shashank-priyadarshi <shashank9163882019@gmail.com>
2023-07-01 14:44:16 +0530

    added template folder, added approx solution to longest substring of non repeat characters

--- leetcode/strings/longest_substr_non_repeat_char/README.md
+++ leetcode/strings/longest_substr_non_repeat_char/README.md
@@ -1,3 +1,10 @@
 # Longest Substring Without Non-repeating Characters
 
 ## Approach 1
+
+- If length of string is == 0, return 0
+- Iterate over the string
+- Maintain a map of unique strings, and length
+- If duplicate element found, empty map
+- If length is greater than length of longest substring, swap
+- Continue till end
--- leetcode/strings/longest_substr_non_repeat_char/solution.go
+++ leetcode/strings/longest_substr_non_repeat_char/solution.go
@@ -1,1 +1,39 @@
 package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(lengthOfLongestSubstring("ciridmtxsgaryv"))
+}
+
+// lengthOfLongestSubstring finds the length of the longest substring without repeating characters.
+//
+// s: The input string.
+// longestSubstr: The length of the longest substring without repeating characters.
+// Returns the length of the longest substring without repeating characters.
+func lengthOfLongestSubstring(s string) (longestSubstr int) {
+	length := len(s)
+	if length <= 1 {
+		return length
+	}
+	longestSubstr = 1
+	substrMap := make(map[string]map[string]int)
+	for _, charRune := range s {
+		charStr := string(charRune)
+		for k, v := range substrMap {
+			delete(substrMap, k)
+			if _, ok := v[charStr]; !ok {
+				k += charStr
+				v[charStr] = 1
+				substrMap[k] = v
+				length = len(k)
+				if length > longestSubstr {
+					longestSubstr = length
+				}
+			}
+		}
+		substrMap[charStr] = make(map[string]int)
+		substrMap[charStr][charStr] = 1
+	}
+	return
+}
